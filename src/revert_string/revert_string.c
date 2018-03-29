ac697ac85e08471d63a7125e198d30822a73dda9
Natalie <blue-tea@list.ru>
2018-03-29 12:36:49 +0000

    last version

--- src/revert_string/revert_string.c
+++ src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int n = strlen(str);
+	for (int i = 0; i< n/2; i++){
+	    char k = str[i];
+	    str[i] = str[n-i-1];
+	    str[n-i-1] = k;
+	}
 }
+
 
